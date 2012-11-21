f1fe0826c82e9d909ccdcc1a1d2a66a3b50559ae
ollyhal <ollyhals@hotmail.co.uk>
2012-11-21 08:22:13 +0000

    21/11/2012

--- BudgetProgram/gen/com/ollyhal/budgetProgram/R.java
+++ BudgetProgram/gen/com/ollyhal/budgetProgram/R.java
@@ -36,6 +36,7 @@ public final class R {
     }
     public static final class layout {
         public static final int newuser=0x7f030000;
+        public static final int pin=0x7f030001;
     }
     public static final class menu {
         public static final int newuser=0x7f060000;
